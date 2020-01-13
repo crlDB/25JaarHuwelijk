e304e0f2d6f9dab4111eba7c50e0caa8f726b498
Carl De Bels <crlDB@users.noreply.github.com>
2020-01-13 19:26:40 +0100

    -

--- app.bundle.js
+++ app.bundle.js
@@ -167,6 +167,24 @@ __webpack_require__(/*! lib_jquerydraggable */ "./node_modules/jquery-ui/ui/widg
 __webpack_require__(/*! ../../node_modules/bootstrap/dist/css/bootstrap.css */ "./node_modules/bootstrap/dist/css/bootstrap.css");
 __webpack_require__(/*! ../../node_modules/font-awesome/css/font-awesome.css */ "./node_modules/font-awesome/css/font-awesome.css");
 __webpack_require__(/*! ../css/app.css */ "./src/css/app.css");
+// create tekst
+var header = $("<h1>OP <span class=\"badge badge-warning\">21MAART</span> <span class=\"badge badge-warning\">2020</span> VIEREN WIJ</br>\n                    ONS <span class=\"badge badge-warning\">25JAAR</span> <span class=\"badge badge-warning\">HUWELIJK</span>, </br>\n                    DIT ......\n\n                </h1>").css('text-align', 'center');
+$('body').append(header);
+// hr
+$('body').append('<hr>');
+// prev - next ------------------------------------------------------------------>
+var answer = $("\n<form class=\"form-inline center\">\n  <button type=\"button\" class=\"btn btn-primary mb-2\">VORIGE</button>\n\n  <div class=\"form-group mx-sm-3 mb-2\">\n    <label for=\"inputPassword2\" class=\"sr-only\">ANTWOORD</label>\n    <input type=\"text\" class=\"form-control\" id=\"inputPassword2\" placeholder=\"antwoord\">\n  </div>\n\n  <button type=\"button\" class=\"btn btn-primary mb-2\">VOLGENDE</button>\n</form>").css('text-align', 'center');
+$('body').append(answer);
+// hr ---------------------------------------------------------------------------->
+$('body').append('<hr>');
+var actief = {
+    vrgNbr: 0,
+};
+var qArr = [];
+var q = { vrg: 'DAG VAN HET FEEST', antw: '17maart' };
+qArr.push(q);
+q = { vrg: 'JAAR VAN HET FEEST', antw: '2020' };
+qArr.push(q);
 ;
 for (var i = 1; i < 10; i++) {
     var y = $('<img>', {
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -12,6 +12,71 @@ import "../css/app.css";
 import * as d3 from "d3";
 
 
+// create tekst
+let header = $(`<h1>OP <span class="badge badge-warning">21MAART</span> <span class="badge badge-warning">2020</span> VIEREN WIJ</br>
+                    ONS <span class="badge badge-warning">25JAAR</span> <span class="badge badge-warning">HUWELIJK</span>, </br>
+                    DIT ......
+
+                </h1>`).css('text-align', 'center');
+$('body').append(header);
+
+
+// hr
+$('body').append('<hr>');
+
+// prev - next ------------------------------------------------------------------>
+let answer = $(`
+<form class="form-inline center">
+  <button type="button" class="btn btn-primary mb-2">VORIGE</button>
+
+  <div class="form-group mx-sm-3 mb-2">
+    <label for="inputPassword2" class="sr-only">ANTWOORD</label>
+    <input type="text" class="form-control" id="inputPassword2" placeholder="antwoord">
+  </div>
+
+  <button type="button" class="btn btn-primary mb-2">VOLGENDE</button>
+</form>`).css('text-align', 'center');
+$('body').append(answer);
+
+
+// hr ---------------------------------------------------------------------------->
+$('body').append('<hr>');
+
+
+let actief = {
+    vrgNbr: 0,
+
+};
+
+
+
+
+let qArr = [];
+let q = { vrg: 'DAG VAN HET FEEST', antw: '17maart'}; qArr.push(q);
+q = { vrg: 'JAAR VAN HET FEEST', antw: '2020' }; qArr.push(q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ;
 
 for (var i = 1; i < 10; i++) {
