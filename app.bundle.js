ecc2476ab7dee1826c4e40a0072b4d63559c8c2d
Carl De Bels <crlDB@users.noreply.github.com>
2020-01-13 20:04:19 +0100

    -

--- app.bundle.js
+++ app.bundle.js
@@ -167,13 +167,17 @@ __webpack_require__(/*! lib_jquerydraggable */ "./node_modules/jquery-ui/ui/widg
 __webpack_require__(/*! ../../node_modules/bootstrap/dist/css/bootstrap.css */ "./node_modules/bootstrap/dist/css/bootstrap.css");
 __webpack_require__(/*! ../../node_modules/font-awesome/css/font-awesome.css */ "./node_modules/font-awesome/css/font-awesome.css");
 __webpack_require__(/*! ../css/app.css */ "./src/css/app.css");
-// create tekst
-var header = $("<h1>OP <span class=\"badge badge-warning\">21MAART</span> <span class=\"badge badge-warning\">2020</span> VIEREN WIJ</br>\n                    ONS <span class=\"badge badge-warning\">25JAAR</span> <span class=\"badge badge-warning\">HUWELIJK</span>, </br>\n                    DIT ......\n\n                </h1>").css('text-align', 'center');
+// create tekst ----------------------------------------------------------------->
+var header = $("<div class=\"alert alert-success\" style=\"text-align:center; margin:0px;\">\n                <h1>OP <span class=\"badge badge-warning\">21MAART</span> <span class=\"badge badge-warning\">2020</span> VIEREN WIJ</br>\n                ONS <span class=\"badge badge-warning\">25JAAR</span> <span class=\"badge badge-warning\">HUWELIJK</span>, </br>\n                DIT\n                </h1>\n                </div>");
 $('body').append(header);
-// hr
-$('body').append('<hr>');
+// hr --------------------------------------------------------------------------->
+//$('body').append('<hr>');
+var vrg = $('<div class="alert alert-primary" role="alert" style="text-align:center; margin:0px;"></div>');
+$('body').append(vrg);
+// hr --------------------------------------------------------------------------->
+//$('body').append('<hr>');
 // prev - next ------------------------------------------------------------------>
-var answer = $("\n<form class=\"form-inline center\">\n  <button type=\"button\" class=\"btn btn-primary mb-2\">VORIGE</button>\n\n  <div class=\"form-group mx-sm-3 mb-2\">\n    <label for=\"inputPassword2\" class=\"sr-only\">ANTWOORD</label>\n    <input type=\"text\" class=\"form-control\" id=\"inputPassword2\" placeholder=\"antwoord\">\n  </div>\n\n  <button type=\"button\" class=\"btn btn-primary mb-2\">VOLGENDE</button>\n</form>").css('text-align', 'center');
+var answer = $("\n<form class=\"form-inline center alert alert-primary\" style=\"text-align:center; margin:0px;\">\n  <button id=\"btnVorige\" type=\"button\" class=\"btn btn-primary mb-2\">VORIGE</button>\n\n  <div class=\"form-group mx-sm-3 mb-2\">\n    <label for=\"inputPassword2\" class=\"sr-only\">ANTWOORD</label>\n    <input type=\"text\" class=\"form-control\" id=\"inputPassword2\" placeholder=\"antwoord\">\n  </div>\n\n  <button id=\"btnVolgende\" type=\"button\" class=\"btn btn-primary mb-2\">VOLGENDE</button>\n</form>").css('text-align', 'center');
 $('body').append(answer);
 // hr ---------------------------------------------------------------------------->
 $('body').append('<hr>');
@@ -185,7 +189,31 @@ var q = { vrg: 'DAG VAN HET FEEST', antw: '17maart' };
 qArr.push(q);
 q = { vrg: 'JAAR VAN HET FEEST', antw: '2020' };
 qArr.push(q);
-;
+q = { vrg: 'h qsdjkfhhjklqshfjkqsh', antw: '2020' };
+qArr.push(q);
+q = { vrg: 'lj dfqsdlfqsdlflkqsjklfq', antw: '2020' };
+qArr.push(q);
+q = { vrg: 'kdfj sdjhfqsdhjkfhqsdklfhklqsfjkqs', antw: '2020' };
+qArr.push(q);
+q = { vrg: 'hfsd sdklfh kqsdjfjkqslhfjkl', antw: '2020' };
+qArr.push(q);
+// btn vorige
+$('#btnVorige').click(function (e) {
+    actief.vrgNbr--;
+    if (actief.vrgNbr < 0)
+        actief.vrgNbr = 0;
+    vrg.empty();
+    vrg.append("<h5>\n                Vraag " + (actief.vrgNbr + 1) + " </br>\n                " + qArr[actief.vrgNbr].vrg + "</h5> </br>\n                (maak puzzel, antwoord staat onderaan)\n                ");
+});
+// btn volgende
+$('#btnVolgende').click(function (e) {
+    actief.vrgNbr++;
+    if (actief.vrgNbr > 10)
+        actief.vrgNbr = 10;
+    vrg.empty();
+    vrg.empty();
+    vrg.append("<h5>\n                Vraag " + (actief.vrgNbr + 1) + " </br>\n                " + qArr[actief.vrgNbr].vrg + "</h5> </br>\n                (maak puzzel, antwoord staat onderaan)\n                ");
+});
 for (var i = 1; i < 10; i++) {
     var y = $('<img>', {
         id: '100_' + i,
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -12,29 +12,34 @@ import "../css/app.css";
 import * as d3 from "d3";
 
 
-// create tekst
-let header = $(`<h1>OP <span class="badge badge-warning">21MAART</span> <span class="badge badge-warning">2020</span> VIEREN WIJ</br>
-                    ONS <span class="badge badge-warning">25JAAR</span> <span class="badge badge-warning">HUWELIJK</span>, </br>
-                    DIT ......
-
-                </h1>`).css('text-align', 'center');
+// create tekst ----------------------------------------------------------------->
+let header = $(`<div class="alert alert-success" style="text-align:center; margin:0px;">
+                <h1>OP <span class="badge badge-warning">21MAART</span> <span class="badge badge-warning">2020</span> VIEREN WIJ</br>
+                ONS <span class="badge badge-warning">25JAAR</span> <span class="badge badge-warning">HUWELIJK</span>, </br>
+                DIT
+                </h1>
+                </div>`);
 $('body').append(header);
 
+// hr --------------------------------------------------------------------------->
+//$('body').append('<hr>');
+let vrg = $('<div class="alert alert-primary" role="alert" style="text-align:center; margin:0px;"></div>');
+$('body').append(vrg);
 
-// hr
-$('body').append('<hr>');
+// hr --------------------------------------------------------------------------->
+//$('body').append('<hr>');
 
 // prev - next ------------------------------------------------------------------>
 let answer = $(`
-<form class="form-inline center">
-  <button type="button" class="btn btn-primary mb-2">VORIGE</button>
+<form class="form-inline center alert alert-primary" style="text-align:center; margin:0px;">
+  <button id="btnVorige" type="button" class="btn btn-primary mb-2">VORIGE</button>
 
   <div class="form-group mx-sm-3 mb-2">
     <label for="inputPassword2" class="sr-only">ANTWOORD</label>
     <input type="text" class="form-control" id="inputPassword2" placeholder="antwoord">
   </div>
 
-  <button type="button" class="btn btn-primary mb-2">VOLGENDE</button>
+  <button id="btnVolgende" type="button" class="btn btn-primary mb-2">VOLGENDE</button>
 </form>`).css('text-align', 'center');
 $('body').append(answer);
 
@@ -54,11 +59,42 @@ let actief = {
 let qArr = [];
 let q = { vrg: 'DAG VAN HET FEEST', antw: '17maart'}; qArr.push(q);
 q = { vrg: 'JAAR VAN HET FEEST', antw: '2020' }; qArr.push(q);
+q = { vrg: 'h qsdjkfhhjklqshfjkqsh', antw: '2020' }; qArr.push(q);
+q = { vrg: 'lj dfqsdlfqsdlflkqsjklfq', antw: '2020' }; qArr.push(q);
+q = { vrg: 'kdfj sdjhfqsdhjkfhqsdklfhklqsfjkqs', antw: '2020' }; qArr.push(q);
+q = { vrg: 'hfsd sdklfh kqsdjfjkqslhfjkl', antw: '2020' }; qArr.push(q);
 
 
 
+// btn vorige
+$('#btnVorige').click((e) => {
+    actief.vrgNbr--;
+    if (actief.vrgNbr < 0)
+        actief.vrgNbr = 0;
 
+    vrg.empty();
+    vrg.append(`<h5>
+                Vraag ${actief.vrgNbr+1} </br>
+                ${qArr[actief.vrgNbr].vrg}</h5> </br>
+                (maak puzzel, antwoord staat onderaan)
+                `);
+})
 
+// btn volgende
+$('#btnVolgende').click((e) => {
+    actief.vrgNbr++;
+    if (actief.vrgNbr > 10)
+        actief.vrgNbr = 10;
+
+    vrg.empty();
+    vrg.empty();
+    vrg.append(`<h5>
+                Vraag ${actief.vrgNbr + 1} </br>
+                ${qArr[actief.vrgNbr].vrg}</h5> </br>
+                (maak puzzel, antwoord staat onderaan)
+                `);
+
+})
 
 
 
